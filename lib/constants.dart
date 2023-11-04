2521af6b94fca0a78e92c22483a8c1207b374fc7
Bilal Sevinç <BilalSevinctr@hotmail.com>
2023-11-04 21:37:37 +0300

    Responsive_Dashboard_UI
    Responsive Dashboard UI

---
+++ lib/constants.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+var defaultBackgroundColor = Colors.grey.shade300;
+var appBarColor = Colors.grey.shade900;
+var myAppBar = AppBar(
+  backgroundColor: appBarColor,
+  title: const Text(' '),
+  centerTitle: false,
+);
+var drawerTextColor = TextStyle(
+  color: Colors.grey.shade600,
+);
+var tilePadding = const EdgeInsets.only(
+  left: 8.0,
+  right: 8,
+  top: 8,
+);
+var myDrawer = Drawer(
+  backgroundColor: Colors.grey.shade300,
+  elevation: 0,
+  child: Column(
+    children: [
+      const DrawerHeader(
+        child: Icon(
+          Icons.favorite,
+          size: 64,
+        ),
+      ),
+      Padding(
+        padding: tilePadding,
+        child: ListTile(
+          leading: const Icon(Icons.home),
+          title: Text(
+            'D A S H B O A R D',
+            style: drawerTextColor,
+          ),
+        ),
+      ),
+      Padding(
+        padding: tilePadding,
+        child: ListTile(
+          leading: const Icon(Icons.settings),
+          title: Text(
+            'S E T T I N G S',
+            style: drawerTextColor,
+          ),
+        ),
+      ),
+      Padding(
+        padding: tilePadding,
+        child: ListTile(
+          leading: const Icon(Icons.info),
+          title: Text(
+            'A B O U T',
+            style: drawerTextColor,
+          ),
+        ),
+      ),
+      Padding(
+        padding: tilePadding,
+        child: ListTile(
+          leading: const Icon(Icons.logout),
+          title: Text(
+            'L O G O U T',
+            style: drawerTextColor,
+          ),
+        ),
+      ),
+    ],
+  ),
+);
